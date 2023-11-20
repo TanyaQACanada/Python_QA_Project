19b04ae50b3b69ea7d6b0cbd3d06d59bd01f42ee
Cmorodinka <tatyanakosachova@gmail.com>
2023-11-20 09:57:44 -0400

    fix set_up fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,13 @@ import pytest
 from playwright.sync_api import sync_playwright, Playwright, Page
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def set_up(playwright):
     browser = playwright.chromium.launch()
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://www.lambdatest.com/selenium-playground")
-    
+
 
     yield page
-    page.close()
-
+    browser.close()
