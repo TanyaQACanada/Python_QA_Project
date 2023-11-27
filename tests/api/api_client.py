3d5d01fa37585cb9b35a349581428834aae23bcd
Cmorodinka <tatyanakosachova@gmail.com>
2023-11-27 12:56:57 -0400

    refactoring: api_client.py

--- tests/api/api_client.py
+++ tests/api/api_client.py
@@ -1,12 +1,12 @@
-import requests
+# import requests
 
 
-class SimpleClient:
-    def __init__(self):
-        self.session = requests.session()
+# class SimpleClient:
+#     def __init__(self):
+#         self.session = requests.session()
 
-    def get(self, endpoint):
-        return self.session.get(f"{'https://jsonplaceholder.typicode.com'}{endpoint}")
+#     def get(self, endpoint):
+#         return self.session.get(f"{'https://jsonplaceholder.typicode.com'}{endpoint}")
 
-    def post(self, endpoint, data):
-        return self.session.post(f"{'https://jsonplaceholder.typicode.com'}{endpoint}", data)
+#     def post(self, endpoint, data):
+#         return self.session.post(f"{'https://jsonplaceholder.typicode.com'}{endpoint}", data)
--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,5 +1,15 @@
 import pytest
-from tests.api.api_client import SimpleClient
+# from tests.api.api_client import SimpleClient
+
+class SimpleClient:
+    def __init__(self):
+        self.session = requests.session()
+
+    def get(self, endpoint):
+        return self.session.get(f"{'https://jsonplaceholder.typicode.com'}{endpoint}")
+
+    def post(self, endpoint, data):
+        return self.session.post(f"{'https://jsonplaceholder.typicode.com'}{endpoint}", data)
 
 
 @pytest.fixture()
