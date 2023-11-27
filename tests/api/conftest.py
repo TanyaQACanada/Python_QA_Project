2098ea8d6133b3eaab7bf8a7350993ececa8d5a2
Cmorodinka <tatyanakosachova@gmail.com>
2023-11-27 13:19:55 -0400

    refactoring: move api tests to tests/api

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -2,16 +2,6 @@ import pytest
 import requests
 from pom.api_client import SimpleClient
 
-class SimpleClient:
-    def __init__(self):
-        self.session = requests.session()
-
-    def get(self, endpoint):
-        return self.session.get(f"{'https://jsonplaceholder.typicode.com'}{endpoint}")
-
-    def post(self, endpoint, data):
-        return self.session.post(f"{'https://jsonplaceholder.typicode.com'}{endpoint}", data)
-
 
 @pytest.fixture()
 def simple_client():
